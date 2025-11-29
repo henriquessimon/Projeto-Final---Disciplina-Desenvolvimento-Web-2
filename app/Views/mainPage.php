7d4d6a401ab9a384df714a48e3ef53d46f096210
Calika <henriquessimon2004@gmail.com>
2025-11-29 18:02:02 -0300

    Adicionando svg de delete no eqp

--- app/Views/mainPage.php
+++ app/Views/mainPage.php
@@ -106,7 +106,8 @@
                                     <li title="Redução de dano de Fogo">RF</li>
                                     <li title="Redução de dano Elétrico">RE</li>
                                     <li title="Estabilidade">Estabilidade</li>
-                                    <li title="Estabilidade" >Favoritar</li>
+                                    <li></li>
+                                    <li></li>
                                 </ul>
                                 <?php foreach  ($cat['items'] as $item):?>
                                     <ul class="status_eqp_list" id="<?=$item['id']?>">
@@ -131,6 +132,14 @@
                                             <svg id="estr_svg" width="30" height="30" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                                                 <polygon points="50,15 61,35 83,38 67,55 70,77 50,67 30,77 33,55 17,38 39,35" 
                                                     fill="<?=$fill_start?>" stroke="orange" stroke-width="2"/>
+                                            </svg>
+                                        </li>
+                                        <li>
+                                            <svg class="dlt_enemy" width="24" height="24" viewBox="0 0 24 24" fill="red" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M3 6H5H21" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                <path d="M8 6V4C8 3.46957 8.21071 2.96086 8.58579 2.58579C8.96086 2.21071 9.46957 2 10 2H14C14.5304 2 15.0391 2.21071 15.4142 2.58579C15.7893 2.96086 16 3.46957 16 4V6M19 6V20C19 20.5304 18.7893 21.0391 18.4142 21.4142C18.0391 21.7893 17.5304 22 17 22H7C6.46957 22 5.96086 21.7893 5.58579 21.4142C5.21071 21.0391 5 20.5304 5 20V6H19Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                <path d="M10 11V17" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                <path d="M14 11V17" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                             </svg>
                                         </li>
                                     </ul>
